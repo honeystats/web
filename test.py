4d7b928ad7a2dd4c0d7136dae45427b2245fc708
caitlin <brown603@msu.edu>
2022-02-24 16:59:05 -0500

    sql parsing update

--- test.py
+++ test.py
@@ -4,8 +4,11 @@ import time
 from urllib.parse import unquote
 import json
 
-hostName = "10.3.0.12"
-serverPort = 8080
+#hostName = "10.3.0.12"
+#serverPort = 8080
+
+hostName = 'localhost'
+serverPort = 80
 
 log = "./log.txt"
 
@@ -82,10 +85,12 @@ class MyServer(SimpleHTTPRequestHandler):
             header[k] = headers[k]
         #print(headers)
 
-        sql_error = check_sql(uname, passwd)
-        if(sql_error!='no'):
+        sql_error_uname = check_sql(uname, uname, passwd)
+        sql_error_passwd = check_sql(passwd, uname, passwd)
+        print('sql uname',sql_error_uname)
+        if(sql_error_uname!='no' or sql_error_passwd!='no'):
             sql = True
-            print(sql_error)
+            print(sql_error_uname, sql_error_passwd)
         else:
             sql = False
 
@@ -117,28 +122,99 @@ class MyServer(SimpleHTTPRequestHandler):
             self.path = '/loginfailed.html'
             SimpleHTTPRequestHandler.do_GET(self)
 
-def check_sql(uname, passwd):
+def parse_sql(command):
+    #if(';' in command):
+    #    return 'multiple_commands'
+    #print('command: ',command)
+    command = command.upper()
+    command = command.replace('+', ' ')
+    command_list = command.split(' ')
+    #print('command list: ', command_list)
+    permissions_statements = ['SELECT','UPDATE','DELETE','DROP','CREATE','ALTER']
+    for statement in permissions_statements:
+        if(statement in command_list):
+            return statement
+    
+    return 'test'
+
+def check_sql(string, uname, passwd):
     #sql query:
     #SELECT * FROM users WHERE username = uname AND password = psw
     #normal entry:
     #SELECT * FROM users WHERE username = 'caitlin' AND password = '1'
     #injection attempt:
-    #SELECT * FROM users WHERE username = 'caitlin' AND password = 'password' OR 1=1 OR 'password' = '1'
+    #SELECT * FROM users WHERE username = 'caitlin' AND password = 'password' OR '1' = '1'
     query = "SELECT * FROM users WHERE username =  AND password = "
     misspell_error = 'Syntax error in SQL statement "SELECT * FROM users WHERE username = ' + uname + ' AND password = ' + passwd + ';"'
     quotes_error = 'Syntax error at or near ";" Position: ' + str(len(query)+len(uname)+len(passwd))
+    permissions_error = 'Error: user does not have '
+    permissions_error_2 = ' permissions'
+    #multiple_commands_error = ''
     generic_error = 'Syntax error in SQL statement'
 
+    #print("string:", string)
+    #print("passwd:", passwd)
 
-    #check for single quotes
-    if (uname.count("'")%2!=0 or passwd.count("'")%2!=0):
+    #remove up until first single quote
+    string_list = string.split("'",1)
+    #passwd_list = passwd.split("'",1)
+    string_1 = string_list[0]
+    #password = passwd_list[0]
+    #print(username, password)
+    #print(type(username), type(password))
+    #print(uname_list, passwd_list)
+    if (len(string_list)==1):
+        #no single quotes
+        return "no"
+    #only keep string before comments
+    comments = ['--','#','/*']
+    string_comment = False
+    #passwd_comment = False
+    for comment in comments:
+        if(comment in string_list[1]):
+            string_comment = True
+            string_list[1] = string_list[1].split(comment)[0]
+        #if(comment in passwd_list[1]);
+        #    passwd_comment = True
+        #    passwd_list[1] = passwd_list[1].split(comment)[0]
+    #remove after last single quote if not commented
+    if (len(string_list)>1 and not string_comment):
+        string_r = string_list[1][::-1]
+        string_list = string_r.split("'",1)
+    elif (not string_comment):
+        uname_list = []
+    #if (len(passwd_list)>1 and not passwd_comment):
+    #    passwd_r = passwd_list[1][::-1]
+    #    passwd_list = passwd_r.split("'",1)
+    #elif (not passwd_comment):
+    #    passwd_list = []
+    #print(uname_list, passwd_list)
+    if (len(string_list)==1):
+        #one single quote, throw error
         return quotes_error
+    #isolate sql command and parse
+    if (len(string_list)>1):
+        #string_command = string_list[1][::-1]
+        string_command = string_list[1]
+        #print(string_command)
+        string_results = parse_sql(string_command)
+    #if(len(passwd_list)>1):
+    #    passwd_command = passwd_list[1][::-1]
+    #    passwd_results = parse_sql(passwd_command)
+    #    print(passwd_command)
+    #check for single quotes
+    #if (uname.count("'")%2!=0 or passwd.count("'")%2!=0):
+    #    return quotes_error
 
     #check double quotes
-    if (uname.count('"')%2!=0 or passwd.count('"')%2!=0):
-        return quotes_error
-
-
+    #if (uname.count('"')%2!=0 or passwd.count('"')%2!=0):
+    #    return quotes_error
+    
+        permissions_statements = ['SELECT','UPDATE','DELETE','DROP','CREATE','ALTER']
+        if(string_results in permissions_statements):
+            return permissions_error+string_results+permissions_error_2
+        else:
+            print(string_results)
     return 'no'
 
 def main():
